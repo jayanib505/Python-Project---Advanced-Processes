132205d0778b2932cea11089fea5b5e59e87dc53
jayanib505 <bhulaja@wgc.school.nz>
2026-08-10 11:35:38 +1200

    Add files via upload

--- AS 91897 - Advanced Processes - Irish learning program.py
+++ AS 91897 - Advanced Processes - Irish learning program.py
@@ -3,20 +3,44 @@
 # Jayani Bhula
 # 30/07/2026
 
+import random
+
 # Title
 print("Haigh Gaeilge\n"
       "Irish learning program\n")
 
-print("""Topics available:
+print("""\n\nTopics available:
   Basic Phrases""")
       
 
 # Have a opening page ask kenisha about that
 
-def learn():
+def learn(basic_phrases):
     print("\nWelcome to Learn")
 
+    topic = input("Enter topic to learn: ").title()
 
+    if topic == "Basic Phrases":
+        print("\nLearn Basic Phrases")
+
+        chosen_word = random.choice(basic_phrases)
+        incorrect_option = random.choice(basic_phrases)
+        correct_answer = random.choice(basic_phrases)
+
+        print(chosen_word)
+        print(incorrect_option['Irish'])
+        print(correct_answer)
+
+        
+        
+        #answer = input("\nEnter the correct translation: ").strip().title()
+        #if answer == "":
+         #   print("Correct!")
+
+        #else:
+         #   print("Incorrect. The correct answer was {}".format)
+
+        
 def dictionary(basic_phrases):
     """Prints dictionaries for chosen topic"""
     print("\nWelcome to the Dictionaries")
@@ -26,12 +50,16 @@ def dictionary(basic_phrases):
 
     if topic == "Basic Phrases":
         print("\nDictionary  - Basic Phrases")
-        # for loop print formatted dictionary
+        # For loop prints formatted dictionary
         for basic in basic_phrases:
             print("English: {:12} | Irish: {:20} |".format
               (basic['English'], basic['Irish']))
 
 
+def profile():
+    print("Profile")
+
+    
 def main():
     """Menu and holds word lists"""
 
@@ -50,6 +78,7 @@ def main():
     # Loop
     while True:
         # Menu
+        print("\n")
         print("\n---Dashboard---")
         print("1. Learn")
         print("2. Dictionary")
@@ -60,10 +89,13 @@ def main():
         decision = input("\nEnter number option: ")
         
         if decision == "1":
-            learn()
+            learn(basic_phrases)
 
         elif decision == "2":
             dictionary(basic_phrases)
+
+        elif decision == "3":
+            profile()
 
         elif decision == "0":
             print("Session over.")
