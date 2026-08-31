9fb7a831daaac60b3c373ace431f3b09e11f286a
jayanib505 <bhulaja@wgc.school.nz>
2026-08-31 12:01:08 +1200

    Add files via upload

--- AS 91897 - Advanced Processes - Irish learning program.py
+++ AS 91897 - Advanced Processes - Irish learning program.py
@@ -18,26 +18,28 @@ print("""\n\nTopics available:
 def learn(basic_phrases):
     print("\nWelcome to Learn")
 
-    # Ask user for topic
     topic = input("Enter topic to learn: ").title()
+    while True:
 
-    # 
-    if topic == "Basic Phrases":
-        dictionary = basic_phrases
-        print("\nLearn Basic Phrases")
+        # If Basic Phrases chosen set as dictionary
+        if topic == "Basic Phrases":
+            dictionary = basic_phrases
 
-        # Select Irish translation 
-        print("Select the correct translation for: Haigh")
+            # Welcome User
+            print("\nLearn Basic Phrases")
 
-        # English translation options
-        print("1. Hello")
-        print("2. Hi")
-        print("3. Bye")
+            # Select Irish translation 
+            print("\nSelect the correct translation for: Haigh")
+
+            # English translation options
+            print("1. Hello")
+            print("2. Hi")
+            print("3. Bye")
 
         # Get user answer selection
         user_selection = input("Enter number selection: ")
 
-        # Check if selection correct
+            # Check if selection correct
         if user_selection == "2":
             print("""Correct! 
             Congratulations on learning a new word!""")
@@ -45,24 +47,147 @@ def learn(basic_phrases):
         else:
             print("Incorrect. Keep going!")
 
-        
 
+
+        # Select Irish translation 
+        print("\nSelect the correct translation for: Dia duit")
+
+        # English translation options
+        print("1. Bye")
+        print("2. Please")
+        print("3. Hello")
+
+        # Get user answer selection
+        user_selection = input("Enter number selection: ")
+
+        # Check if selection correct
+        if user_selection == "3":
+            print("""Correct! 
+            Congratulations on learning a new word!""")
+
+        else:
+            print("Incorrect. Keep going!")
+
+
+
+         # Select Irish translation 
+        print("\nSelect the correct translation for: Slán")
+
+        # English translation options
+        print("1. Bye")
+        print("2. Thank you")
+        print("3. Please")
+
+        # Get user answer selection
+        user_selection = input("Enter number selection: ")
+
+            # Check if selection correct
+        if user_selection == "1":
+            print("""Correct! 
+            Congratulations on learning a new word!""")
+
+        else:
+            print("Incorrect. Keep going!")
+
+
+
+        # Select Irish translation 
+        print("\nSelect the correct translation for: Le do thoil")
+
+        # English translation options
+        print("1. Hi")
+        print("2. Welcome")
+        print("3. Please")
+
+        # Get user answer selection
+        user_selection = input("Enter number selection: ")
+
+            # Check if selection correct
+        if user_selection == "3":
+            print("""Correct! 
+            Congratulations on learning a new word!""")
+
+        else:
+            print("Incorrect. Keep going!")
+
+
+
+        # Select Irish translation 
+        print("\nSelect the correct translation for: Go raibh maith agat")
+
+        # English translation options
+        print("1. Thank you")
+        print("2. Welcome")
+        print("3. Bye")
+
+        # Get user answer selection
+        user_selection = input("Enter number selection: ")
+
+            # Check if selection correct
+        if user_selection == "1":
+            print("""Correct! 
+            Congratulations on learning a new word!""")
+
+        else:
+            print("Incorrect. Keep going!")
+
+
+
+        # Select Irish translation 
+        print("\nSelect the correct translation for: Maidin mhaith")
+
+        # English translation options
+        print("1. Hello")
+        print("2. Good Morning")
+        print("3. Welcome")
+
+        # Get user answer selection
+        user_selection = input("Enter number selection: ")
+
+            # Check if selection correct
+        if user_selection == "2":
+            print("""Correct! 
+            Congratulations on learning a new word!""")
+
+        else:
+            print("Incorrect. Keep going!")
+
+
+
+        # Select Irish translation 
+        print("\nSelect the correct translation for: Fáilte")
+
+        # English translation options
+        print("1. Bye")
+        print("2. Please")
+        print("3. Welcome")
+
+        # Get user answer selection
+        user_selection = input("Enter number selection: ")
+
+            # Check if selection correct
+        if user_selection == "3":
+            print("""Correct! 
+            Congratulations on learning a new word!""")
+
+        else:
+            print("Incorrect. Keep going!")
+
+
+        
 
 def dictionary(basic_phrases):
     """Prints dictionaries for chosen topic"""
     print("\nWelcome to the Dictionaries")
 
-    # Ask user for topic
+    #Ask user for topic
     topic = input("Enter dictionary topic: ").title()
 
-    # Check if basic phrases entered
     if topic == "Basic Phrases":
         print("\nDictionary  - Basic Phrases")
-
         # For loop prints formatted dictionary
         for basic in basic_phrases:
             print(basic)
-
 
 def profile():
     print("Profile")
@@ -74,10 +199,10 @@ def main():
     # List of dictionaries - Topic: Basic Phrases
     basic_phrases = [
         {"Hi" : "Haigh"},
-        {"Hello" : "Dia Duit"},
+        {"Hello" : "Dia duit"},
         {"Bye" : "Slán"},
         {"Please" : "Le do thoil"},
-        {"Thankyou" : "Go raibh maith agat"},
+        {"Thank you" : "Go raibh maith agat"},
         {"Welcome" : "Fáilte"},
         {"Good Morning" : "Maidin mhaith"},
         {"With" : "le"},
